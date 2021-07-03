80d762e54843dfeaabdd3bb795fabca4dbc0690c
skybldev <skybluehexapon@outlook.com>
2021-07-03 13:38:50 -0400

    testing release 1
    - refactored a TON of stuff
    - no more prompts, all options
    - parallelization!
    - automatic FPS detection!
    - and more

--- README.md
+++ README.md
@@ -1,66 +1,60 @@
 # video-lqr
 A bash script that liquid rescales videos
 
-*This repository was forked from my alternate GitHub account.*
+*This repository was forked from my alternate GitHub account. It lives on here.*
 
-This does not use Photoshop or GIMP, just a **Linux environment**, **ffmpeg** and **imagemagick**. If you don't have Linux, you can install cygwin or use WSL (not tested on there though!).
+This does not use Photoshop or GIMP, just a **Linux environment**, **FFmpeg** and **ImageMagick**. If you don't have Linux, you can install Cygwin or use WSL. (not tested on there though!)
 
 ### What is Seam Carving/Liquid Rescaling/Content Aware Scale?
 
 Seam carving, which was named Content Aware Scale and made accessible by Adobe through Photoshop, is a method of image rescaling by carving "seams" around areas of (guessed) importance in order to preserve them while removing pixels on that path. It is "content-aware," thus the name. Learn more about it here: https://en.wikipedia.org/wiki/Seam_carving. It is also known as liquid rescaling. It's made its way into meme culture because of the funny ways it can change images.
 
-Usually when you want to perform that on an image you would use Photoshop's Content Aware Scale tool. However, the algorithm for doing so is actually public and has been ported onto various other software. One of which is ImageMagick, a terminal-based, powerful image manipulation tool. (https://imagemagick.org/index.php) It has implemented seam carving. 
+Usually when you want to perform that on an image you would use Photoshop's Content Aware Scale tool. However, the algorithm for doing so is actually public and has been ported onto various other software. One of which is ImageMagick, a terminal-based, powerful image manipulation tool. (https://imagemagick.org/index.php)
 
 ### What is this?
 
-This script that automates that process, by:
+This script automates that process, by:
 
-1. Splitting a video into its individual frames as .png files using ffmpeg
-2. Running imagemagick's `-liquid-rescale` on all of them
+1. Splitting a video into its individual frames as .png files using FFmpeg
+2. Running ImageMagick's `-liquid-rescale` on all of them
 3. Parsing all of the resized frames into a video (along with the audio)
 
-The second step takes VERY long by itself, but is much, much faster than doing it in Photoshop. 
+The second step takes VERY long by itself, but is much, much faster than doing it in Photoshop.
 
 ### Documentation
 
-#### Basic Usage
+#### Quick Start
 
-In order to use the script, place it into the folder where you want it to work in. Preferrably one with the source video in it. After that, simply run the script using `./video-lqr.sh`. The script is interactive when used without arguments, so it will ask you for all the information it needs:
+*Sorry if this sounds stupid to you experienced Bash developers; I am writing this for beginners. If you have any suggestions, please create an Issue. :)*
 
-1. Source video
-2. Output video<br>
-   *The source and output video are self explanatory, but you will not be able to tab-complete due to the nature of bash.*
-3. Frames per second<br>
-   *Since the program is not able to determine the source video's FPS (yet), you will have to enter it manually. You can find it using VLC's metadata information feature, or `ffprobe`.*
-4. Output resolution<br>
-   *This is the resolution to which you want to scale each of the frames. It can either be `WITDHxHEIGHT` <sup>e.g. 1600x900</sup> or a percentage of the original resolution.*
+Download the script manually or by cloning this repository. Run it using `./video-lqr.sh`. If it does not execute, make sure you have correct permissions and `chmod a+x video-lqr.sh` just to be sure.
 
-After you enter said information, it will start working. It will usually take a while to convert all the frames. If you entered something by mistake or want to halt the script for some reason, hit Ctrl+C.
+Make sure you read the helptext first (pass `--help`) to get to know what each argument does. You will learn more from those than this Quick Start.
 
-When it finishes creating the new video, it will ask if you want to delete the frames and other files it created during the process. Saying no will keep those files if you want to use them later.
+To convert a video, simply run:
+
+```bash
+./video.lqr -i <your input video> -r <the output resolution>
+```
+
+...and it will start working! It will save to a video of the same name as the input, in your current directory. If you want it to save to a custom file, simply pass `-o <the output filename>`.
 
 #### Advanced Usage
 
-##### Arguments
+##### Processing high-framerate or very long videos
 
-This script supports arguments in a specific order.
+As with everything, your environment will have its limits. Namely, it will have an argument limit. (see yours with `getconf ARG_MAX`) This is relevant to this script because each frame of a video is an argument that counts towards that limit.
 
-`./video-lqr.sh [input] [output] [outputRes] [fps]
+If you are working with a video that is (usually) over an hour long, or is at an absurdly high framerate, it can start to hit that limit. To avoid hitting it and increase your wiggle room, it is advisable to use a shorter temporary directory name. It is more advisable to split your video if it's very long, or decrease its framerate if it's high.
 
-* **[input]** path to input video
-* **[output]** path to output video
-* **[outputRes]** a `WIDTHxHEIGHT` or percentage of the source resolution
-* **[fps]** the fps of the source video
+### TODO
 
-If none are entered then the script will act interactively. (See Basic Usage)
-
-##### Reusing Frames
-
-The script creates a temporary directory at startup if it doesn't exist. If files are found in it, it will ask whether or not it should delete the files, and if not, whether or not to process those files. If you choose yes to processing those files, it will skip the initial steps of splitting the video into individual frames and will immediately start processing the frames of the previous source video.
-
-This works granted the frames from the previous run have not been deleted.
-
-### Upcoming Features
-
-* Automatically determining a video's FPS
+* ~~Automatically determining a video's FPS~~ (Done)
 * An option to include custom audio
+* ~~Multithreading the slow ImageMagick process (Using GNU Parallel possibly?)~~ (Done)
+* Better output resolution notation checking
+* Splitting videos into chunks using FFmpeg to avoid hitting ARG_MAX (Messy and will do later)
+
+### Special thanks
+
+...to [u/kevors](https://www.reddit.com/user/kevors) for giving me a stern talking to on Reddit, giving helpful advice and suggestions (and code), and motivating me to get off my butt and actually make this script decent. :)
--- video-lqr.sh
+++ video-lqr.sh
@@ -1,118 +1,236 @@
-#!/usr/bin/env bash
-inputFile=$1        # input file
-outputFile=$2       # output file
-outputRes=$3        # output resolution in WIDTHxHEIGHT
-fps=$4              # fps for both input and output
-useLeftoverFrames=0 # whether or not to process already split frames
+#!/usr/bin/bash
+input=""            # input video
+output=""           # output video
 
-# create temp dir if it doesn't exist yet
-if [[ ! -d ".lqrtemp/" ]]; then
-  echo ":: Creating temporary directory .lqrtemp/..."
-  mkdir .lqrtemp/
-fi
+outputRes=""        # output resolution in WIDTHxHEIGHT
+fps=0               # fps for both input and output
+tempDir=".lqrtemp/" # temporary directory to store frames and work files
+keepFrames=0        # whether or not to delete frames upon finishing
+maxThreads=4        # maximum number of threads to use
+parallel=0          # whether or not to parallelize the process
 
-# ask if user wants to delete leftover files from last run if found
-if [ "$(ls -A .lqrtemp/)" ]; then
-  while true; do
-    read -r -p ">> Leftover frames from the last run were found. Delete? [Y/n] " answer
-    if [[ $answer == [yY] ]] || [ -z "$answer" ]; then
-      echo ":: Deleting leftover frames..."
-      rm -rf .lqrtemp/
-    else
-      while true; do
-        read -r -p ">> Would you like to process those frames? [Y/n] " answer1
-        if [[ $answer1 =~ [yY] ]] || [[ -z "$answer1" ]]; then
-          echo ":: Will use leftover frames from last run."
-          useLeftoverFrames=1
-        else
-          echo ":: Doing nothing."
-          exit 0
-        fi
-        break
-      done
-    fi
-    break
-  done
-fi
+err () {
+  printf '!! %s\n' "$1"
+}
 
-# prompt user for input file if not entered
-if [ -z "$inputFile" ] && [ $useLeftoverFrames == 0 ]; then
-  while true; do
-    read -r -p ">> Please enter the path to the source video: " answer
-    inputFile=$answer
-    break;
-  done
-fi
+warn () {
+  printf '!: %s\n' "$1"
+}
 
-# prompt user for output file if not entered
-if [ -z "$outputFile" ]; then
-  while true; do
-    read -r -p ">> Please enter the path to the output video: " answer
-    outputFile=$answer
-    break;
-  done
-fi
+ok () {
+  printf ':: %s\n' "$1"
+}
 
-# prompt user for FPS if not entered
-if [ -z "$fps" ]; then
-  while true; do
-    read -r -p ">> Please enter source video's FPS: " answer
-    echo ":: Processing $inputFile at $answer FPS."
-    fps=$answer
-    break;
-  done
-fi
+# -- Process options --
 
-# prompt user for output resolution if not entered
-if [ -z "$outputRes" ]; then
-  while true; do
-    read -r -p ">> Please enter the desired output resolution: " answer
-    outputRes=$answer
-    break;
-  done
-fi
+while test $# -gt 0; do
+  case "$1" in
+    -h|--help)
+      echo "video-lqr - a bash script that liquid rescales videos"
+      echo " "
+      echo "video-lqr [options]"
+      echo " "
+      echo "options:"
+      echo "-i, --input             Input video."
+      echo "-o, --output            Output video."
+      echo "-h, --help              Show this help."
+      echo "-r, --resolution SIZE   Specify the resolution of the output. Can be"
+      echo "                        WIDTHxHEIGHT or a percentage of the input's resolution."
+      echo "-f, --framerate FPS     Specify a custom framerate."
+      echo "--temp-directory PATH   Specify a directory to store temporary files in."
+      echo "--keep-frames           Keep frames upon finishing."
+      echo "--parallel              Enables parallel processing using GNU Parallel. This"
+      echo "                        often makes the ImageMagick part of the process much"
+      echo "                        faster depending on how many cores your processor has."
+      echo "--max-threads THREADS   Specify the maximum threads that GNU Parallel will use."
+      exit 0
+      ;;
+    -i|--input)
+      shift
+      input=$1
+      shift
+      ;;
+    -o|--output)
+      shift
+      output=$1
+      shift
+      ;;
+    -r|--resolution)
+      shift
+      outputRes=$1
+      shift
+      ;;
+    -f|--framerate)
+      shift
+      fps=$1
+      shift
+      ;;
+    --temp-directory)
+      shift
+      tempDir=$1
+      shift
+      ;;
+    --keep-frames)
+      keepFrames=1
+      shift
+      ;;
+    --parallel)
+      parallel=1
+      shift
+      ;;
+    --max-threads)
+      shift
+      maxThreads=$1
+      shift
+      ;;
+    *)
+      err "Unrecognized option $1."
+      exit 1
+      break
+      ;;
+  esac
+done
 
-# split video into individual frames and extract audio
-if [ $useLeftoverFrames == 0 ]; then
-  echo ":: Processing $inputFile."
-  echo ":: Splitting frames into .lqrtemp/ ..."
-  ffmpeg -v panic -i "$inputFile" -vcodec png .lqrtemp/%05d.png
-  echo ":: Extracting audio from $inputFile into .lqrtemp/.audio.aac..."
-  ffmpeg -v panic -i "$inputFile" -vn -acodec copy .lqrtemp/.audio.aac
+# -- Check stuff --
+
+# Check input file
+if [ "$input" = "" ]; then
+  err "Please provide input file with -i INPUT."
+  exit 1
+elif [ -d "$input" ]; then
+  # TODO: accept directory of frames if supplied one
+  err "Input cannot be a directory."
+  exit 1
+elif ! [ -a "$input" ]; then
+  err "Invalid input path."
+  exit 1
 else
-  echo ":: Processing frames directly from .lqrtemp/ ..."
+  ok "Using $input"
 fi
 
-# apply lqr to the images
-echo ":: Liquid resizing frames to $outputRes..."
-frameCount=$(find .lqrtemp -mindepth 1 -maxdepth 1 ! -name resized | wc -l)
-[[ ! -d ".lqrtemp/resized/" ]] && mkdir .lqrtemp/resized/
-for file in .lqrtemp/*
-  do
-    if [[ ! -d $file ]]; then
-      convert "$file" -liquid-rescale "$outputRes" ".lqrtemp/resized/${file##*/}"
-      printf ":: Scaled %s out of %s \\r" "${file##*/}" "$frameCount"
-    fi
-done
+# Check output path
+if [ "$output" = "" ]; then
+  output=$(basename "$input")
+fi
+ok "Will save to $output"
 
-# convert frames back into video
-echo ":: Parsing resized frames into video..."
-ffmpeg -v panic -framerate "$fps" -i .lqrtemp/resized/%05d.png ".lqrtemp/.noAudio.mp4"
+# Check output resolution
+if [ "$outputRes" = "" ]; then
+  err "Please provide output resolution with -s RESOLUTION."
+  exit 1
+else
+  ok "Output resolution set to $outputRes"
+fi
 
-# put audio on video
-echo ":: Adding audio track..."
-ffmpeg -i ".lqrtemp/.noAudio.mp4" -i ".lqrtemp/.audio.aac" -c:v copy -c:a aac -b:a 256k "$outputFile"
+# Determine framerate
+if [ "$fps" -lt 0 ]; then
+  err "Invalid framerate."
+  exit 1
+elif [ "$fps" = 0 ]; then
+  # Thank you kevors for the command.
+  ffmpegOut=$(ffprobe \
+    -v error \
+    -select_streams v \
+    -show_entries stream=r_frame_rate \
+    -of default=nw=1:nk=1 \
+    "$input")
 
-# asks user whether or not to delete work files
-while true; do
-  read -r -p ">> Would you like to delete the leftover frames? [Y/n] " answer
-  if [[ $answer =~ [yY] ]] || [[ -z "$answer" ]]; then
-    echo ":: Deleting..."
-    rm -rf .lqrtemp
-  else
-    echo ":: Keeping files."
+  if [ $? = 1 ]; then
+    err "FFmpeg errored above!"
+    exit 1
   fi
-  break
-done
 
-echo ":: Done."
+  fps=$((ffmpegOut))
+  ok "Determined ${fps} FPS from input."
+else
+  ok "Processing at $fps FPS."
+fi
+
+# Trim trailing whitespace from tempDir path
+tempDir=${tempDir%/}
+
+# Create temp dir if it doesn't exist yet
+if ! [ -d "$tempDir" ]; then
+  warn "Creating temporary directory $tempDir..."
+  mkdir "$tempDir"
+fi
+
+# Check if GNU Parallel is installed and maxthreads if --parallel is passed
+if [ $parallel = 1 ]; then
+  command -v "parallel"
+
+  if [ $? = 1 ]; then
+    err "GNU Parallel was not found on this system. Exiting."
+    exit 1
+  fi
+
+  if [ "$maxThreads" -le 0 ]; then
+    err "Max thread count must be above 0."
+    exit 1
+  fi
+fi
+
+# -- Actual process --
+
+# Error out if leftover files from last run are found, otherwise split
+if [ "$(find "$tempDir" -mindepth 1 -print -quit)" ]; then # Passes if non-empty
+  err "Leftover files found. Exiting."
+  exit 1
+else
+  ok "Splitting frames into $tempDir..."
+
+  # Sincere note:
+  # I kindly do not care about the argument limit right now. Who is going to
+  # be liquid rescaling 75 minute long 25 FPS videos? Who will want to watch
+  # a comedically liquid rescaled 75 minute long video? What is more, who
+  # will be liquid rescaling a >120 FPS video? It's already jarring to watch
+  # one at 60 FPS. I have a few ideas on how to get around this but it will
+  # be a huge mess to try to implement it especially considering FFmpeg's
+  # weird caveats concerning splitting videos frame-perfectly. For now I will
+  # keep it simple and if someone, somewhere raises the issue, I will
+  # implement it. I hope you understand. Thank you. :)
+  ffmpeg -v panic -i "$input" -vcodec png "$tempDir/%05d.png"
+fi
+
+# Check if resized directory exists
+if ! [ -d "$tempDir/resized" ]; then
+  mkdir "$tempDir/resized"
+fi
+
+# Apply lqr to the images
+if [ $parallel = 0 ]; then
+  ok "Liquid resizing frames to $outputRes..."
+  
+  mogrify -monitor -path "$tempDir/resized/" -liquid-rescale "$outputRes!" "$tempDir/*.png"
+else
+  ok "Liquid resizing frames to $outputRes... (Parallelizing to up to $maxThreads threads)"
+
+  find "$tempDir" -maxdepth 1 -type f -name "*.png" | \
+    parallel -k mogrify -monitor -path "$tempDir/resized/" -liquid-rescale "$outputRes!" "{}"
+fi
+
+if [ $? = 1 ]; then
+  err "ImageMagick errored above!"
+  exit 1
+fi
+
+# Finalize video
+ok "Parsing frames and adding audio to video..."
+ffmpeg \
+  -v error \
+  -framerate "$fps" \
+  -i "$tempDir/resized/%05d.png" \
+  -i "$input" \
+  -map 0:v:0 \
+  -map 1:a:0 \
+  "$output"
+
+# Cleanup if needed
+if [ $keepFrames = 1 ]; then
+  ok "Keeping all frames."
+else
+  ok "Cleaning up..."
+  rm -rf "$tempDir"
+fi
+
+ok "Done."
